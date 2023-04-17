7278f6b0a6d1ad5d21b489e9e172807af7a4358e
sucremage40 <clement_1712@live.fr>
2023-04-17 18:21:30 +0200

    Main

--- first_test.py
+++ first_test.py
@@ -1,7 +1,7 @@
 import panda as pd
 import matplotlib.pyplot as plt
 
-datasets = []
+ds15 = ["datasets/2015S1_NB_FER.csv", "datasets/2015S1_PROFIL_FER.csv", "datasets/2015S2_NB_FER.csv", "datasets/2015S2_PROFIL_FER.csv"]
 
 def import_dataset(data):
-    return
+    return 
