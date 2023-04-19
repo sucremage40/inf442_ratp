2291c14ea4dfdc326187482a048560ec11e53e84
sucremage40 <clement_1712@live.fr>
2023-04-19 23:48:18 +0200

    txt 2016 not working

--- dataset.py
+++ dataset.py
@@ -18,5 +18,5 @@ class Dataset:
 
 
 def import_dataset(data):
-    current_dataset = pd.read_csv(data, sep=" ") #sep = ';' for 15, " " for 16 and +
+    current_dataset = pd.read_csv(data, sep="\t", lineterminator="\r") #sep = ';' for 15, " " for 16 and +
     return current_dataset
